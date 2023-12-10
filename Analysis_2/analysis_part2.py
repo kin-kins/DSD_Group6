ba956befb1ea30f98b9ac1fa24377bf94cbdeac4
AYYANARK <aayanmasters@gmail.com>
2023-12-10 13:27:18 -0500

    es index insertions

--- Analysis_2/analysis_part2.py
+++ Analysis_2/analysis_part2.py
@@ -1,42 +1,62 @@
 import pandas as pd
 import json
+import sys
+import os
+from elasticsearch import Elasticsearch
+from opensearchpy import OpenSearch
 
+username = 'admin'
+password = 'Elasticsearch123!#'
+index_name = 'analysis2'
+http_auth = (username, password)
+hosts = 'https://search-dsdgroup6-pt4vzj4nnlkksrgfvv6rmiuxgu.us-east-1.es.amazonaws.com:443'
+es = OpenSearch(hosts=hosts, http_auth=http_auth)
 class Category_Analysis:
-    def extract_min_and_max_category_count(self,csv_file_path):
+    def extract_category_count(self,csv_file_path,year):
         data = pd.read_csv(csv_file_path)
-        category_counts = data.groupby('categoryId').size().reset_index(name='counts')
-        # print(category_counts)
-        max_count = category_counts['counts'].max()
-        category_max = category_counts.loc[category_counts['counts'] == max_count, 'categoryId'].values
-        # Find the minimum count
-        min_count = category_counts['counts'].min()
-        category_min = category_counts.loc[category_counts['counts'] == min_count, 'categoryId'].values
-        # print(f"Category ID(s) with maximum count ({max_count}): {category_max}")
-        # print(f"Category ID(s) with minimum count ({min_count}): {category_min}")
-        return category_min,category_max
+        data['trending_date'] = pd.to_datetime(data['trending_date'], format='%Y-%m-%dT%H:%M:%SZ').dt.tz_localize(None)
+        final_data=data[data['trending_date'].dt.year==year]
+        category_counts = final_data.groupby('categoryId').size().reset_index(name='counts')
+        return category_counts
     def fetch_category_name_from_json(self,json_file_path):
 
         # Read the JSON file
         with open(json_file_path,'r') as json_file:
             data = json.load(json_file)
-
-        category_min_id = category_min[0]  # Replace with your category min ID
-        category_max_id = category_max[0]  # Replace with your category max ID
-
-        titles = {}
+        sorted_counts = category_counts.sort_values(by='counts', ascending=False)
+        json_dict = {}
         for item in data['items']:
-            if item['id'] == str(category_min_id):
-                titles['min'] = item['snippet']['title']
-            elif item['id'] == str(category_max_id):
-                titles['max'] = item['snippet']['title']
-        return titles
+            json_dict[item['id']] = item['snippet']['title']
 
+        list_of_dicts = sorted_counts.to_dict(orient='records')
+        for dictionary in list_of_dicts:
+            dictionary['year'] = 2022
+        for item in list_of_dicts:
+            category_id = str(item['categoryId'])  # Convert to string to match keys in json_dict
+            if category_id in json_dict:
+                item['category_name'] = json_dict[category_id]
+            else:
+                item['category_name'] = 'Unknown'
+
+        for item in list_of_dicts:
+            item['ID'] = f"{item['categoryId']}_{item['year']}"
+
+        print(list_of_dicts)
+        for single_dict in list_of_dicts:
+            try:
+                response = es.index(index=index_name, id=single_dict["ID"], body=single_dict)
+                print(f"Document inserted successfully. Document ID: {response['_id']}")
+            except Exception as e:
+                print(f"Error inserting document: {e}")
 
 if __name__ == '__main__':
-    csv_path = 'C:/Users/aayan/Desktop/Fall 2023/DSD/Project/DSD Youtube Dataset/preprocessed_single_file_dataset.csv'
-    json_path='C:/Users/aayan/Desktop/Fall 2023/DSD/Project/DSD Youtube Dataset/BR_category_id.json'
+    #csv_path = 'C:/Users/aayan/Desktop/Fall 2023/DSD/Project/DSD Youtube Dataset/preprocessed_single_file_dataset.csv'
+    #json_path='C:/Users/aayan/Desktop/Fall 2023/DSD/Project/DSD Youtube Dataset/BR_category_id.json'
+    dir = os.path.dirname(__file__)
+    csv_path = os.path.join(dir, 'preprocessed_single_file_dataset.csv')
+    json_path=os.path.join(dir, 'BR_category_id.json')
+    year=int(sys.argv[1])
+    #year=2023
     category_obj=Category_Analysis()
-    category_min,category_max=category_obj.extract_min_and_max_category_count(csv_path)
+    category_counts=category_obj.extract_category_count(csv_path,year)
     category_name=category_obj.fetch_category_name_from_json(json_path)
-    print(f"Category name of least preferred video: {category_name.get('min')}")
-    print(f"Category name of most preferred video: {category_name.get('max')}")
