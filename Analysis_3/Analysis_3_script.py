422d8ba8b20a52466101dc656ddaf46c4a93103b
jbdou <douglasj0811@gmail.com>
2023-12-11 22:16:58 -0500

    Analysis 3 Changes

--- Analysis_3/Analysis_3_script.py
+++ Analysis_3/Analysis_3_script.py
@@ -1,24 +1,41 @@
 import pandas as pd
+import json
+import sys
+import os
+from elasticsearch import Elasticsearch
+from opensearchpy import OpenSearch
 
-# Read the CSV file containing all the data
-file_path = 'C:\\Users\\jbdou\\Downloads\\preprocessed_single_file_dataset.csv'
-df = pd.read_csv(file_path)
+pd.options.mode.chained_assignment = None
 
-# Convert 'trending_date' column to datetime
-df['trending_date'] = pd.to_datetime(df['trending_date'], format='%Y-%m-%dT%H:%M:%SZ')
+# Specify your Elasticsearch cluster credentials
+username = 'admin'
+password = 'Elasticsearch123!#'
+index_name = 'analysis3'
+http_auth = (username, password)
+hosts = 'https://search-dsdgroup6-pt4vzj4nnlkksrgfvv6rmiuxgu.us-east-1.es.amazonaws.com:443'
 
-# Create a new column for the month and year
-df['month'] = df['trending_date'].dt.month
-df['year'] = df['trending_date'].dt.year
+# Create an Elasticsearch client with authentication
+es = OpenSearch(hosts=hosts, http_auth=http_auth)
 
-# Initialize an empty DataFrame to store results
-result_df = pd.DataFrame()
 
-# Iterate through each year and month
-for year in df['year'].unique():
-    for month in df['month'].unique():
-        # Filter data for the specific year and month
-        monthly_data = df[(df['year'] == year) & (df['month'] == month)]
+def get_common_trending_videos_for_year(df, input_year):
+    # Convert 'trending_date' column to datetime
+    df['trending_date'] = pd.to_datetime(df['trending_date'], format='%Y-%m-%dT%H:%M:%SZ')
+
+    # Create 'year' and 'month' columns
+    df['year'] = df['trending_date'].dt.year
+    df['month'] = df['trending_date'].dt.month
+
+    # Filter data for the specified year
+    year_data = df[df['year'] == input_year]
+
+    # Initialize an empty DataFrame to store results
+    result_df = pd.DataFrame()
+
+    # Iterate through each month
+    for month in year_data['month'].unique():
+        # Filter data for the specific month
+        monthly_data = year_data[year_data['month'] == month]
 
         # Group by video_id and aggregate relevant information
         grouped_common_trending_videos = monthly_data.groupby('video_id').agg({
@@ -48,10 +65,47 @@ for year in df['year'].unique():
         # Append results to the overall result DataFrame
         result_df = pd.concat([result_df, common_trending_videos_selected])
 
-# Sort the result by 'trending_date'
-result_df = result_df.sort_values(by='trending_date')
+    # Sort the result by 'trending_date'
+    result_df = result_df.sort_values(by='trending_date')
 
-# Display the final sorted result
-print("\nCommon Trending Videos for Each Whole Month Among All Countries (Sorted by Trending Date):")
-print(result_df)
+    return result_df
 
+
+def main():
+    try:
+        # Read the CSV file containing all the data
+        # file_path = 'C:\\Users\\jbdou\\Downloads\\preprocessed_single_file_dataset.csv'
+        # df = pd.read_csv(file_path)
+        dir = os.path.dirname(__file__)
+        df = os.path.join(dir, 'preprocessed_single_file_dataset.csv')
+
+        # Take the input year from the command line argument
+        input_year = int(sys.argv[1])
+
+        # Get the common trending videos for the specified year
+        result_for_year = get_common_trending_videos_for_year(df, input_year)
+
+        # Convert the result to JSON format
+        result_json = result_for_year.to_json(orient='records', date_format='iso', default_handler=str)
+
+        # Convert the JSON string to a list
+        result_list = json.loads(result_json)
+
+        # Print the list
+        print(f"\nCommon Trending Videos for {input_year} (Sorted by Trending Date) in List format:")
+        #print(result_list)
+        for entry in result_list:
+            entry["year"] = input_year  # Add the input year to each entry
+            try:
+                response = es.index(index=index_name, id=entry["id"], body=entry)
+                print(f"Document inserted successfully. Document ID: {response['_id']}")
+            except Exception as e:
+                print(f"Error inserting document: {e}")
+
+
+    except IndexError:
+        print("Please provide the year as a command line argument.")
+
+
+if __name__ == "__main__":
+    main()
