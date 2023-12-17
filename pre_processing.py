53d68d2592d07d7ec28d79f70dfa6f2b1756be80
Vishnu Ramesh <vishnuramesh200@gmail.com>
2023-12-17 00:36:16 -0500

    Update pre_processing.py
    Updated pre processing file

--- pre_processing.py
+++ pre_processing.py
@@ -1,21 +1,22 @@
 import os
 import pandas as pd
 
-def preprocess_csv(input_file, output_file, columns_to_drop,filename):
+def preprocess_csv(input_file, output_file,columns_to_drop,filename):
     data = pd.read_csv(input_file)
     data.drop(columns=columns_to_drop, inplace=True)
     data['country_name'] = filename[:2]
     data.to_csv(output_file, index=False)
 
-directory = 'C:\\Users\\vishy\\OneDrive\\Desktop\\CONCORDIA\\COMP6231\\PROJECT\\'
+directory = os.path.dirname(__file__)
+print(directory)
 all_data = []
 for filename in os.listdir(directory):
     if filename.endswith('.csv'):
         input_csv = os.path.join(directory, filename)
+        columns_to_drop = ['comments_disabled','ratings_disabled', 'description']
         output_csv = os.path.join(directory, f"processed_{filename}")
         print(filename[:2])
-        columns_to_drop = ['tags', 'comment_count', 'thumbnail_link', 'comments_disabled','ratings_disabled', 'description']
-        preprocess_csv(input_csv, output_csv, columns_to_drop,filename)
+        preprocess_csv(input_csv, output_csv,columns_to_drop,filename)
         df = pd.read_csv(output_csv)
         all_data.append(df)
 
